76918bcbee8722c4cea59f08238340b76521faef
saykara <saykara@itu.edu.tr>
2017-12-08 00:18:56 +0300

    added get ingredient name and user ingredient add, update and list functions

--- ingredient.py
+++ ingredient.py
@@ -9,6 +9,14 @@ class IngredientParameter:
         self.name = name
 
 
+class Ingredient:
+    def __init__(self, id, userId, ingredientId, amount):
+        self.id = id
+        self.userId = userId
+        self.ingredientId = ingredientId
+        self.amount = amount
+
+
 class IngredientDatabase:
     @classmethod
     def addIngredientParameter(cls, name):
@@ -40,6 +48,27 @@ class IngredientDatabase:
                 return -1
 
     @classmethod
+    def getIngredientName(cls,id):
+        with dbapi2.connect(database.config) as connection:
+            cursor = connection.cursor()
+            query = """SELECT * FROM IngredientParameter WHERE ID = %s""" %id
+
+            try:
+                cursor.execute(query,(id))
+                ingredientInfo = cursor.fetchone()
+            except dbapi2.Error:
+                connection.rollback()
+            else:
+                connection.commit()
+
+            cursor.close()
+
+            if ingredientInfo:
+                return IngredientParameter(ID=ingredientInfo[0], name=ingredientInfo[1], )
+            else:
+                return -1
+
+    @classmethod
     def deleteIngredientParameter(cls, id):
         with dbapi2.connect(database.config) as connection:
             cursor = connection.cursor()
@@ -54,3 +83,53 @@ class IngredientDatabase:
             connection.commit()
 
         cursor.close()
+
+
+class IngredientMapDatabase:
+    @classmethod
+    def addIngredient(cls, userID, ingredientID, amount):
+        with dbapi2.connect(database.config) as connection:
+            cursor = connection.cursor()
+            query = """INSERT INTO IngredientMap (userID,ingredientID,amount) VALUES (%s,%s,%s)"""
+            cursor.execute(query, (userID,ingredientID,amount))
+            cursor.close()
+
+    @classmethod
+    def getAllIngredientsOfUser(cls):
+        with dbapi2.connect(database.config) as connection:
+            cursor = connection.cursor()
+            userID = current_user.id
+            query = """SELECT * FROM IngredientMap WHERE UserID = %s"""%userID
+
+            try:
+                cursor.execute(query,(userID))
+                ingredientInfo = cursor.fetchall()
+            except dbapi2.Error:
+                connection.rollback()
+            else:
+                connection.commit()
+
+            cursor.close()
+
+            if ingredientInfo:
+                return IngredientParameter(ID=ingredientInfo[0], userId=ingredientInfo[1], ingredientId=ingredientInfo[2], amount=ingredientInfo[3],)
+            else:
+                return -1
+
+    @classmethod
+    def updateIngredientOfUser(cls, newAmount):
+        with dbapi2.connect(database.config) as connection:
+            cursor = connection.cursor()
+            userID = current_user.id
+            try:
+                for i in newAmount:
+                    query = """UPDATE IngredientMap SET Amount = '%s' WHERE UserID = %s and IngredientID = %s""" %(i[1],userID,i[0])
+                    cursor.execute(query)
+
+
+            except dbapi2.Error:
+                connection.rollback()
+            else:
+                connection.commit()
+                cursor.close()
+            return True
