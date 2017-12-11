44af721b11edcab796ba7ab57a937b7d0cee812a
saykara <saykara@itu.edu.tr>
2017-12-11 20:04:50 +0300

    added update user test

--- ProfileTest.py
+++ ProfileTest.py
@@ -1,5 +1,7 @@
 import unittest
+from flask_login import login_user, current_user, login_required, logout_user
 
+from passlib.apps import custom_app_context as pwd_context
 from Profile import Profile
 from user import UserLogin
 
@@ -20,8 +22,11 @@ class ProfileTest(unittest.TestCase):
         self.assertEqual(userInfoList[2], userInfo[0][2])
         self.assertEqual(userInfoList[3], userInfo[0][5])
 
-    
-
+    def test_UpdateUserInfo(self):
+        userInfoList = ["testName", "testSurame", "test@itu.edu.tr", "test123", "test14"]
+        UserLogin.add_user(userInfoList[0], userInfoList[1], userInfoList[2], userInfoList[3], userInfoList[4])
+        updatedInfoList = ["testNameUpdate", "testSurameUpdate", "test@itu.edu.trUpdate", "",""]
+        Profile.update_userInfo(2, updatedInfoList)
 
 
 if __name__ == '__main__':
